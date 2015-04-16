cc8951a458bd59f47fa706c6bf4e386c0736df6c
Ilinca <ilinca.ispas@gmail.com>
2015-04-16 15:43:40 +0300

    Initial Commit

---
+++ FunctionalSwift/FunctionalSwift.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalSwift.h
+//  FunctionalSwift
+//
+//  Created by Ilinca on 16/04/2015.
+//  Copyright (c) 2015 iosnsagora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalSwift.
+FOUNDATION_EXPORT double FunctionalSwiftVersionNumber;
+
+//! Project version string for FunctionalSwift.
+FOUNDATION_EXPORT const unsigned char FunctionalSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalSwift/PublicHeader.h>
+
+
---
+++ FunctionalSwiftTests/FunctionalSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  FunctionalSwiftTests.swift
+//  FunctionalSwiftTests
+//
+//  Created by Ilinca on 16/04/2015.
+//  Copyright (c) 2015 iosnsagora. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FunctionalSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
